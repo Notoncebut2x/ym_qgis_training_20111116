124034051b3fc2c2c1ea717f05b38524a8bd2a97
notoncebut2x <rpnealon@gmail.com>
2021-11-16 16:33:47 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# QGIS Training Presentation for YouthMappers Sierra Leone
---
+++ orgchart/thelab.js
@@ -0,0 +1,61 @@
+{
+  "nodes": [
+    {
+      "name": "Ayan",
+      "group": 1
+    },
+    {
+      "name": "Amanda",
+      "group": 1
+    },
+    {
+      "name": "Daniel",
+      "group": 1
+    },
+    {
+      "name": "Kolja",
+      "group": 1
+    },
+    {
+      "name": "Paul",
+      "group": 1
+    },
+    {
+      "name": "Rory",
+      "group": 1
+    },
+    {
+      "name": "Violet",
+      "group": 1
+    },
+    
+  ],
+  "links": [
+    {
+      "source": 1,
+      "target": 0,
+      "value": 1
+    },
+    {
+      "source": 2,
+      "target": 0,
+      "value": 8
+    },
+    {
+      "source": 3,
+      "target": 0,
+      "value": 10
+    },
+    {
+      "source": 3,
+      "target": 2,
+      "value": 6
+    },
+    {
+      "source": 4,
+      "target": 0,
+      "value": 1
+    }
+
+  ]
+}
